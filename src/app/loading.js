2db5ff87da789434fc6c67d5c0ede638b92c204c
Kanishk <67541211+Kanishk2004@users.noreply.github.com>
2024-08-04 02:27:44 +0530

    re-structured code

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,12 +1,17 @@
 import Image from 'next/image';
-import styles from './page.module.css';
 
 const loading = () => {
-  return (
-    <div className={styles.loadingContainer}>
-      <Image src={'/loading.gif'} alt='loading' width={50} height={50} unoptimized/>
-    </div>
-  )
-}
+	return (
+		<div className={'loadingContainer'}>
+			<Image
+				src={'/loading.gif'}
+				alt="loading"
+				width={50}
+				height={50}
+				unoptimized
+			/>
+		</div>
+	);
+};
 
-export default loading
+export default loading;
---
+++ src/components/heroPage/HeroPage.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import styles from './heroPage.module.css';
+
+const HeroPage = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.textContainer}>
+				<h1>dishy - The Flavor Network</h1>
+				<h3>Home page</h3>
+			</div>
+			<div className={styles.imageContainer}>
+				<Image
+					src={'/recipe-illustration.png'}
+					alt="hero image"
+					width={500}
+					height={500}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default HeroPage;
