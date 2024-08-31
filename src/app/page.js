7d5b97ad843e436e584f7c3e81034afbdafb8228
Kanishk <67541211+Kanishk2004@users.noreply.github.com>
2024-09-01 00:32:02 +0530

    work in progress for howItWorks section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import FeatureSection from '@/components/heroPage/featureSection/FeatureSection';
 import styles from './page.module.css';
 import HeroPage from '@/components/heroPage/HeroPage';
+import HowItWorks from '@/components/heroPage/howItWorks/HowItWorks';
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<HeroPage />
 			<FeatureSection />
+			<HowItWorks />
 		</div>
 	);
 }
