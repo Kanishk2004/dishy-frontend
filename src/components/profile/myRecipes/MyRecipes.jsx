eba60c6739421ae04ffb20d3b05bab588557b157
Kanishk <67541211+Kanishk2004@users.noreply.github.com>
2024-09-05 15:28:18 +0530

    Add my recipes section to profile

--- src/components/profile/myRecipes/MyRecipes.jsx
+++ src/components/profile/myRecipes/MyRecipes.jsx
@@ -2,6 +2,7 @@
 import { useRecipe } from '@/context/RecipeContext';
 import styles from './myRecipes.module.css';
 import { useEffect } from 'react';
+import MyRecipeCard from './myRecipeCard/MyRecipeCard';
 
 const MyRecipes = () => {
 	const { myRecipies, fetchUserRecipies } = useRecipe();
@@ -13,13 +14,13 @@ const MyRecipes = () => {
 
 	return (
 		<div className={styles.container}>
-			<h4>My Recipes</h4>
+			<h4>My Recipes - ({myRecipies?.length}) </h4>
 			<div className={styles.recipeCardContainer}>
 				{(!myRecipies || myRecipies.length === 0) && (
 					<h3>You posted 0 recipies</h3>
 				)}
 				{myRecipies?.map((recipe) => (
-					<h5 key={recipe?._id}>{recipe?.title}</h5>
+					<MyRecipeCard recipe={recipe} key={recipe?._id} />
 				))}
 			</div>
 		</div>
